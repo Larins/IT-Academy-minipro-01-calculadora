6d84b42943d4657a4ee923423a4afc436134018e
Lara Saiz <69240139+Larins@users.noreply.github.com>
2021-09-13 12:10:52 +0200

    corrección Pi y núm. E

--- src/MiniCalculadora/MiniCalculadora.java
+++ src/MiniCalculadora/MiniCalculadora.java
@@ -4,18 +4,25 @@ import java.util.Scanner;
 public class MiniCalculadora {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+	String Pi = Double.toString(Math.PI);
+	String E = Double.toString(Math.E);
+    
     System.out.print("Indica la operación: [+] [-] [x] [/] [%] ");
-    String in = sc.next();
-    char oper = in.charAt(0);
+    String entrada = sc.next();
+    char oper = entrada.charAt(0);
 
-    System.out.print("Introduce un número: ");
-    in = sc.next();
-    double num1 = Double.parseDouble(in);
+    System.out.print("Introduce un número: [x] [Pi] [E]\n\t[x] = el número que escojas\n\t[Pi] = " + Pi + "\n\t[E] = " + E);
+    entrada = sc.next();
+    if (entrada.equals("Pi")) {entrada = Pi;}
+    if (entrada.equals("E")) {entrada = E;}
+    double num1 = Double.parseDouble(entrada);
 
-    System.out.print("Introduce otro número: ");
-    in = sc.next();
-    double num2 = Double.parseDouble(in);
-
+    System.out.print("Introduce otro número: [y] [Pi] [E]\n\t[y] = el número que escojas\n\t[Pi] = " + Pi + "\n\t[E] = " + E);
+    entrada = sc.next();
+    if (entrada.equals("Pi")) {entrada = Pi;}
+    if (entrada.equals("E")) {entrada = E;}
+    double num2 = Double.parseDouble(entrada);
+    
     if(oper == '+') {
         double resultado = num1 + num2;
         System.out.println("SUMA");
